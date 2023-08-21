ef3c44573d2ab922e12cb9403988d7a0c4e863ab
Shreegandh-CM-xworkz <shreegandhcm.xworkz@gmail.com>
2023-08-21 12:08:11 +0530

    Method

---
+++ Division.java
@@ -0,0 +1,31 @@
+class D{
+   public static void div(float a ,float b){
+           System.out.println(a/b);
+		   }
+		 }
+class Division{
+   
+     public static void main (String args[]){
+	   D obj=new D();
+	   obj.div(5,7);
+	    obj.div(7,9);
+		 obj.div(8,8);
+		obj.div(9,7);
+		obj.div(10,65);
+		obj.div(12,61);
+		obj.div(13,16);
+		 obj.div(5,6);
+		 obj.div(5,8);
+		 obj.div(6,6);
+		obj.div(5,9);
+		obj.div(5,6);
+	   obj.div(51,7);
+	     obj.div(59,69);
+	    obj.div(87,6);
+	       obj.div(59,56);
+	      obj.div(44,77);
+		obj.div(58,66);
+		 obj.div(98,0);
+	    obj.div(98,67);
+	  }
+	 }
---
+++ Substraction.java
@@ -0,0 +1,31 @@
+class S{
+   public static void sub(int a ,int b){
+           System.out.println(a-b);
+		   }
+		 }
+class Substraction{
+   
+     public static void main (String args[]){
+	   S obj=new S();
+	   obj.sub(5,7);
+	    obj.sub(7,0);
+		 obj.sub(8,8);
+		obj.sub(9,7);
+		obj.sub(10,65);
+		obj.sub(12,61);
+		obj.sub(13,16);
+		 obj.sub(5,0);
+		 obj.sub(5,8);
+		 obj.sub(6,6);
+		obj.sub(5,9);
+		obj.sub(5,6);
+	   obj.sub(51,7);
+	  obj.sub(59,69);
+	  obj.sub(87,6);
+	  obj.sub(59,56);
+	   obj.sub(44,77);
+		obj.sub(58,66);
+		 obj.sub(98,0);
+	    obj.sub(98,67);
+	  }
+	 }
