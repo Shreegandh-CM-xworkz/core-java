4a2275ad90b804136fcc3f6b2c283e3ec65df327
Shreegandh-CM-xworkz <shreegandhcm.xworkz@gmail.com>
2023-08-21 12:01:47 +0530

    my third upload

---
+++ Addition.java
@@ -0,0 +1,31 @@
+
+class Addition {
+
+	public static void main(String args[]) {
+
+		add(5, 7);
+		add(7, 0);
+		add(8, 8);
+		add(9, 7);
+		add(10, 65);
+		add(12, 61);
+		add(13, 16);
+		add(5, 0);
+		add(5, 8);
+		add(6, 6);
+		add(5, 9);
+		add(5, 6);
+		add(51, 7);
+		add(59, 69);
+		add(87, 6);
+		add(59, 56);
+		add(44, 77);
+		add(58, 66);
+		add(98, 0);
+		add(98, 67);
+	}
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+}
