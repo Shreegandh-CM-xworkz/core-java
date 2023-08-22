c452e0cf84d43d202ca2ade93ef8b75e34af056f
Shreegandh-CM-xworkz <shreegandhcm.xworkz@gmail.com>
2023-08-22 11:38:19 +0530

    methods

---
+++ Factorial.java
@@ -0,0 +1,30 @@
+
+class Fact{
+   public static void getFac(int FactorialNumber){
+    int fact=1;
+	for(int j:arr[i]){
+   for(int factNum=1;factNum<=j;factNum++){
+      fact=fact*factNum;
+	  }
+	
+	  System.out.println("Factorial of factNumber "+ factNumber +" is " +fact);
+	}
+   }
+}
+
+
+class Factorial{
+public static void main(String args[]){
+	
+	
+	
+	 
+   Fact obj=new Fact();
+   obj.getFac();
+ 
+	  
+	  }
+	  
+	 
+	}
+  
---
+++ Multiplication.java
@@ -0,0 +1,31 @@
+class M{
+   public static void multiply(int a ,int b){
+           System.out.println(a*b);
+		   }
+		 }
+class Multiplication{
+   
+     public static void main (String args[]){
+	   M obj=new M();
+	   obj.multiply(5,7);
+	    obj.multiply(7,0);
+		 obj.multiply(8,8);
+		obj.multiply(9,7);
+		obj.multiply(10,65);
+		obj.multiply(12,61);
+		obj.multiply(13,16);
+		 obj.multiply(5,0);
+		 obj.multiply(5,8);
+		 obj.multiply(6,6);
+		obj.multiply(5,9);
+		obj.multiply(5,6);
+	   obj.multiply(51,7);
+	     obj.multiply(59,69);
+	    obj.multiply(87,6);
+	       obj.multiply(59,56);
+	      obj.multiply(44,77);
+		obj.multiply(58,66);
+		 obj.multiply(98,0);
+	    obj.multiply(98,67);
+	  }
+	 }
