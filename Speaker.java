a11f02dae286a5462ee7ab63348c553fcbe44968
Shreegandh-CM-xworkz <shreegandhcm.xworkz@gmail.com>
2023-08-22 11:34:35 +0530

    variables

---
+++ Speaker.java
@@ -0,0 +1,10 @@
+class Speaker{
+    static String one="hii";
+	static String one="hii";
+	public static void main(String speaker[]){
+	System.out.println("Main started");
+	
+	System.out.println("Main ended");
+	}
+}
+	
--- Tool.java
+++
@@ -1,9 +0,0 @@
-class Tool {
-	public static void main(String[] args) {
-		String salesPackage = "The set contains 13 Pieces 1/4 inch DR Sockets from 4mm to 14 mm, 1 Piece 1/4 inch DR Bit Adaptor, 21 Pieces 1/4 inch DR Bit Sockets, 3 Pieces Hex Key Wrenches, 1 Piece Universal joint, 1 piece Sliding T bar, 2 Pieces Extension Bars, 1 piece Quick Ratchet Handle, 1 Piece Flexible Extension and 1 Piece Spinner Handle";
-		String modelNumber = "46-1";
-		System.out.println(salesPackage);
-		System.out.println(modelNumber);
-
-	}
-}
