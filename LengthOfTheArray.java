b9c1a8041832020cacec8efbce3631df2b28c770
Shreegandh-CM-xworkz <shreegandhcm.xworkz@gmail.com>
2023-09-06 14:48:19 +0530

    parameterised constructor

---
+++ LengthOfTheArray.java
@@ -0,0 +1,9 @@
+class LengthOfTheArray{
+public static void main(String args[]){
+
+  int input[]={ 12,3,2,45,6,7,6};
+  
+   System.out.println( input.length+" is the length of the array");
+  
+}
+}
